f36e8a8661636fa683e1043e28bc35a0f6fb5a49
valerio <romavalerioio@gmail.com>
2021-08-08 18:48:16 +0100

    comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nasa-earth
+
+Retrieve images for the supplied location and date from the Landsat 8 satellite through the [NASA Earth API](https://api.nasa.gov/).
--- main.js
+++ main.js
@@ -10,24 +10,28 @@ submit.disabled = true;
 const image = document.querySelector('img');
 let [errorMessage, latitude, longitude] = ['', NaN, NaN];
 
-const zFill = number => number < 10 ? '0' + number : number;
+// set today's date as max value and default value for the date input
+const zeroFill = number => number < 10 ? '0' + number : number;
 (function setTodayDate() {
     const today = new Date();
-    const yearMonthDate = `${today.getFullYear()}-${zFill(today.getMonth() + 1)}-${zFill(today.getDate())}`;
+    const yearMonthDate = `${today.getFullYear()}-${zeroFill(today.getMonth() + 1)}-${zeroFill(today.getDate())}`;
     date.max = date.value = yearMonthDate;
 })();
 
+// remove the table from the DOM
 function removeTable(data) {
     if (document.querySelector('table'))
         table.remove();
 }
 
+// insert the table in the DOM and updates all the values
 function createTable(data) {
     removeTable();
     table.querySelectorAll('td:last-child').forEach(element => element.innerText = data[element.id]);
     form.insertAdjacentElement('afterEnd', table);
 }
 
+// return an Array of three values: an error (if present else ''), latitude and longitude (if available else NaN)
 async function getCoordinates(path) {
     const response = await fetch(`https://api.postcodes.io/${path}`);
     const data = await response.json();
@@ -41,6 +45,7 @@ async function getCoordinates(path) {
     }
 }
 
+// return an url for the image fetched from the NASA API
 async function getImage() {
     const dim = dimensions.value && dimensions.validity.valid ? dimensions.value : 1;
     const response = await fetch(`https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&dim=${dim}&date=${date.value}&api_key=DEMO_KEY`);
@@ -48,12 +53,14 @@ async function getImage() {
     return URL.createObjectURL(blob);
 }
 
+// handle blur on the postcode input and click on the random button
 async function handleBlurClick(path) {
     [errorMessage, latitude, longitude] = await getCoordinates(path);
     error.innerText = errorMessage;
     submit.disabled = Boolean(errorMessage);
 }
 
+// handle click on the submit buttons, and show the image
 async function handleClick() {
     image.src = await getImage();
 }
